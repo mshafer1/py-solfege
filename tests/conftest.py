a60cbccceaa4950649c5e1293323c4bf171c3c05
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2024-10-05 11:17:31 -0500

    format

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,6 @@
 
 import pytest_readme
 
-# pytest_readme.setup creates a test_readme.py that contains the contents 
+# pytest_readme.setup creates a test_readme.py that contains the contents
 # of the readme for pytest to pick up the doctests
 pytest_readme.setup()
