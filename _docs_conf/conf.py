763ffccf734f39c3ccde6e3883803f9cd4b91a91
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2024-10-05 11:08:33 -0500

    fix linter issues

--- _docs_conf/conf.py
+++ _docs_conf/conf.py
@@ -5,36 +5,36 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import autoapi
 
-project = 'solfege'
-copyright = '2024, mshafer1'
-author = 'mshafer1'
+project = "solfege"
+copyright = "2024, mshafer1"
+author = "mshafer1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-    'sphinx_mdinclude',
-    'sphinx.ext.autodoc.typehints',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "sphinx_mdinclude",
+    "sphinx.ext.autodoc.typehints",
 ]
 
-extensions.append('autoapi.extension')
+extensions.append("autoapi.extension")
 
-autoapi_dirs = ['../solfege']
-autodoc_typehints = 'description'
-templates_path = ['_templates']
-autoapi_template_dir = '_templates/autoapi'
+autoapi_dirs = ["../solfege"]
+autodoc_typehints = "description"
+templates_path = ["_templates"]
+autoapi_template_dir = "_templates/autoapi"
 
-import autoapi
 
 autoapi_options = list(autoapi.extension._DEFAULT_OPTIONS)
-autoapi_options.remove('private-members')
+autoapi_options.remove("private-members")
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 html_theme = "sphinx_rtd_theme"
